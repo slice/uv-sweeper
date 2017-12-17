ed0d11b988c97fb172063991555dfdbf8e718bfa
slice <ryaneft@gmail.com>
2017-12-17 12:47:18 -0800

    show vote count and status

--- client/src/Idea.js
+++ client/src/Idea.js
@@ -2,29 +2,48 @@ import React from 'react'
 
 import './Idea.css'
 
-export default props => (
-  <div className="idea">
-    <button
-      type="button"
-      className="master-button"
-      title="Use as master"
-      onClick={props.onAssignMaster.bind(null, props.id)}
-    >
-      *
-    </button>
-    <button
-      type="button"
-      className="add-button"
-      title="Add to inferiors"
-      onClick={props.onAddToInferior.bind(null, props.id)}
-    >
-      +
-    </button>
-    <h1>
-      <a href={props.url} target="_blank" rel="noopener">
-        {props.title}
-      </a>
-    </h1>
-    <p>{props.description}</p>
-  </div>
-)
+const colors = {
+  completed: '#53fa00',
+}
+
+export default props => {
+  const votes = new Intl.NumberFormat().format(props.votes)
+
+  return (
+    <div className={`idea idea-status-${props.status}`}>
+      <button
+        type="button"
+        className="master-button"
+        title="Use as master"
+        onClick={props.onAssignMaster.bind(null, props.id)}
+      >
+        *
+      </button>
+      <button
+        type="button"
+        className="add-button"
+        title="Add to inferiors"
+        onClick={props.onAddToInferior.bind(null, props.id)}
+      >
+        +
+      </button>
+      {props.status != null && props.status !== 'none' ? (
+        <div
+          className="status"
+          style={{ background: colors[props.status] || 'gray' }}
+        >
+          {props.status}
+        </div>
+      ) : null}
+      <h1>
+        <a href={props.url} target="_blank" rel="noopener">
+          {props.title}
+        </a>
+      </h1>
+      <strong className="vote-count">
+        {votes} vote{props.votes !== 1 ? 's' : ''}
+      </strong>
+      <p>{props.description}</p>
+    </div>
+  )
+}
